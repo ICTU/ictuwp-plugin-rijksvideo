5daaf2a4ef97b330596420e22e1885cf6e8b04f2
Paul van Buuren <paul@wbvb.nl>
2020-03-04 02:07:15 +0100

    file locations sh scripts bijgewerkt

--- scripts/get_cmb2_files.sh
+++ scripts/get_cmb2_files.sh
@@ -1,48 +1,48 @@
 
 
-# sh '/shared-paul-files/Webs/git-repos/ICTU---Digitale-Overheid-WP---rijksvideoplugin/get_cmb2_files.sh' &>/dev/null
+# sh '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictuwp-plugin-rijksvideo/scripts/get_cmb2_files.sh' &>/dev/null
 
 
 # copy to temp dir
-rsync -r -a --delete '/shared-paul-files/Webs/git-repos/CMB2/' '/shared-paul-files/Webs/temp/'
+rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/git-repos/CMB2/' '/Users/paul/shared-paul-files/Webs/temp/'
 
 # clean up temp dir
-rm -rfv '/shared-paul-files/Webs/temp/.git/'
-rm -rfv '/shared-paul-files/Webs/temp/.github/'
-rm '/shared-paul-files/Webs/temp/.gitignore'
-rm '/shared-paul-files/Webs/temp/.gitattributes'
-rm '/shared-paul-files/Webs/temp/.scrutinizer.yml'
-rm '/shared-paul-files/Webs/temp/.travis.yml'
-rm '/shared-paul-files/Webs/temp/config.codekit'
-rm '/shared-paul-files/Webs/temp/distribute.sh'
-rm '/shared-paul-files/Webs/temp/README.md'
-rm '/shared-paul-files/Webs/temp/CHANGELOG.md'
-rm '/shared-paul-files/Webs/temp/CONTRIBUTING.md'
-rm '/shared-paul-files/Webs/temp/readme.txt'
-rm '/shared-paul-files/Webs/temp/LICENSE'
-rm '/shared-paul-files/Webs/temp/Gruntfile.js'
-rm '/shared-paul-files/Webs/temp/package.json'
-rm '/shared-paul-files/Webs/temp/composer.json'
-rm '/shared-paul-files/Webs/temp/Dockunit.json'
-rm '/shared-paul-files/Webs/temp/coverage.clover'
-rm '/shared-paul-files/Webs/temp/phpunit.xml.dist'
-rm '/shared-paul-files/Webs/temp/example-functions.php'
+rm -rfv '/Users/paul/shared-paul-files/Webs/temp/.git/'
+rm -rfv '/Users/paul/shared-paul-files/Webs/temp/.github/'
+rm '/Users/paul/shared-paul-files/Webs/temp/.gitignore'
+rm '/Users/paul/shared-paul-files/Webs/temp/.gitattributes'
+rm '/Users/paul/shared-paul-files/Webs/temp/.scrutinizer.yml'
+rm '/Users/paul/shared-paul-files/Webs/temp/.travis.yml'
+rm '/Users/paul/shared-paul-files/Webs/temp/config.codekit'
+rm '/Users/paul/shared-paul-files/Webs/temp/distribute.sh'
+rm '/Users/paul/shared-paul-files/Webs/temp/README.md'
+rm '/Users/paul/shared-paul-files/Webs/temp/CHANGELOG.md'
+rm '/Users/paul/shared-paul-files/Webs/temp/CONTRIBUTING.md'
+rm '/Users/paul/shared-paul-files/Webs/temp/readme.txt'
+rm '/Users/paul/shared-paul-files/Webs/temp/LICENSE'
+rm '/Users/paul/shared-paul-files/Webs/temp/Gruntfile.js'
+rm '/Users/paul/shared-paul-files/Webs/temp/package.json'
+rm '/Users/paul/shared-paul-files/Webs/temp/composer.json'
+rm '/Users/paul/shared-paul-files/Webs/temp/Dockunit.json'
+rm '/Users/paul/shared-paul-files/Webs/temp/coverage.clover'
+rm '/Users/paul/shared-paul-files/Webs/temp/phpunit.xml.dist'
+rm '/Users/paul/shared-paul-files/Webs/temp/example-functions.php'
 
 
 
-cd '/shared-paul-files/Webs/temp/'
+cd '/Users/paul/shared-paul-files/Webs/temp/'
 find . -name ‘*.DS_Store’ -type f -delete
 
 
 # copy to rijksvideoplugin dir
-rsync -r -a --delete '/shared-paul-files/Webs/temp/' '/shared-paul-files/Webs/git-repos/ICTU---Digitale-Overheid-WP---rijksvideoplugin/cmb2/' 
+rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/temp/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictuwp-plugin-rijksvideo/cmb2/' 
 
 # copy to volwassenheidsscore dir
-rsync -r -a --delete '/shared-paul-files/Webs/temp/' '/shared-paul-files/Webs/git-repos/ICTU---GC-volwassenheidsscore-plugin/cmb2/' 
+rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/temp/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/themes/gebruiker-centraal/development/plugins/ictuwp-plugin-maturityscore/cmb2/' 
 
 # copy to Planning-Tool dir
-rsync -r -a --delete '/shared-paul-files/Webs/temp/' '/shared-paul-files/Webs/git-repos/Digitale-Overheid---WordPress-plugin-Planning-Tool/cmb2/' 
+rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/temp/' '/Users/paul/shared-paul-files/Webs/git-repos/Digitale-Overheid---WordPress-plugin-Planning-Tool/cmb2/' 
 
 
 # remove temp dir
-rm -rfv '/shared-paul-files/Webs/temp/'
+rm -rfv '/Users/paul/shared-paul-files/Webs/temp/'
--- scripts/get_mediaelement_files.sh
+++ scripts/get_mediaelement_files.sh
@@ -2,47 +2,47 @@
 
 
 # copy to temp dir
-rsync -r -a -v --delete '/shared-paul-files/Webs/git-repos/mediaelement/' '/shared-paul-files/Webs/temp/'
+rsync -r -a -v --delete '/Users/paul/shared-paul-files/Webs/git-repos/mediaelement/' '/Users/paul/shared-paul-files/Webs/temp/'
 
 # clean up temp dir
-rm -rfv '/shared-paul-files/Webs/temp/.git/'
-rm -rfv '/shared-paul-files/Webs/temp/.github/'
-rm -rfv '/shared-paul-files/Webs/temp/demo/'
-rm '/shared-paul-files/Webs/temp/.gitignore'
-rm '/shared-paul-files/Webs/temp/.gitattributes'
-rm '/shared-paul-files/Webs/temp/.scrutinizer.yml'
-rm '/shared-paul-files/Webs/temp/.travis.yml'
-rm '/shared-paul-files/Webs/temp/config.codekit'
-rm '/shared-paul-files/Webs/temp/distribute.sh'
-rm '/shared-paul-files/Webs/temp/README.md'
-rm '/shared-paul-files/Webs/temp/CHANGELOG.md'
-rm '/shared-paul-files/Webs/temp/CONTRIBUTING.md'
-rm '/shared-paul-files/Webs/temp/readme.txt'
-rm '/shared-paul-files/Webs/temp/LICENSE'
-rm '/shared-paul-files/Webs/temp/Gruntfile.js'
-rm '/shared-paul-files/Webs/temp/package.json'
-rm '/shared-paul-files/Webs/temp/composer.json'
-rm '/shared-paul-files/Webs/temp/Dockunit.json'
-rm '/shared-paul-files/Webs/temp/coverage.clover'
-rm '/shared-paul-files/Webs/temp/phpunit.xml.dist'
-rm '/shared-paul-files/Webs/temp/example-functions.php'
-rm '/shared-paul-files/Webs/temp/api.md'
-rm '/shared-paul-files/Webs/temp/bower.json'
-rm '/shared-paul-files/Webs/temp/compile_swf.sh'
-rm '/shared-paul-files/Webs/temp/guidelines.md'
-rm '/shared-paul-files/Webs/temp/installation.md'
-rm '/shared-paul-files/Webs/temp/TODO.md'
-rm '/shared-paul-files/Webs/temp/usage.md'
+rm -rfv '/Users/paul/shared-paul-files/Webs/temp/.git/'
+rm -rfv '/Users/paul/shared-paul-files/Webs/temp/.github/'
+rm -rfv '/Users/paul/shared-paul-files/Webs/temp/demo/'
+rm '/Users/paul/shared-paul-files/Webs/temp/.gitignore'
+rm '/Users/paul/shared-paul-files/Webs/temp/.gitattributes'
+rm '/Users/paul/shared-paul-files/Webs/temp/.scrutinizer.yml'
+rm '/Users/paul/shared-paul-files/Webs/temp/.travis.yml'
+rm '/Users/paul/shared-paul-files/Webs/temp/config.codekit'
+rm '/Users/paul/shared-paul-files/Webs/temp/distribute.sh'
+rm '/Users/paul/shared-paul-files/Webs/temp/README.md'
+rm '/Users/paul/shared-paul-files/Webs/temp/CHANGELOG.md'
+rm '/Users/paul/shared-paul-files/Webs/temp/CONTRIBUTING.md'
+rm '/Users/paul/shared-paul-files/Webs/temp/readme.txt'
+rm '/Users/paul/shared-paul-files/Webs/temp/LICENSE'
+rm '/Users/paul/shared-paul-files/Webs/temp/Gruntfile.js'
+rm '/Users/paul/shared-paul-files/Webs/temp/package.json'
+rm '/Users/paul/shared-paul-files/Webs/temp/composer.json'
+rm '/Users/paul/shared-paul-files/Webs/temp/Dockunit.json'
+rm '/Users/paul/shared-paul-files/Webs/temp/coverage.clover'
+rm '/Users/paul/shared-paul-files/Webs/temp/phpunit.xml.dist'
+rm '/Users/paul/shared-paul-files/Webs/temp/example-functions.php'
+rm '/Users/paul/shared-paul-files/Webs/temp/api.md'
+rm '/Users/paul/shared-paul-files/Webs/temp/bower.json'
+rm '/Users/paul/shared-paul-files/Webs/temp/compile_swf.sh'
+rm '/Users/paul/shared-paul-files/Webs/temp/guidelines.md'
+rm '/Users/paul/shared-paul-files/Webs/temp/installation.md'
+rm '/Users/paul/shared-paul-files/Webs/temp/TODO.md'
+rm '/Users/paul/shared-paul-files/Webs/temp/usage.md'
 
-DO NOT CHANGE THESE FILES. USE -src- FOLDER
+# DO NOT CHANGE THESE FILES. USE -src- FOLDER
 
 
-cd '/shared-paul-files/Webs/temp/'
-find . -name ‘*.DS_Store’ -type f -delete
+cd '/Users/paul/shared-paul-files/Webs/temp/'
+find . -name '*.DS_Store' -type f -delete
 
 
 # copy to temp dir
-rsync -r -a -v --delete '/shared-paul-files/Webs/temp/' '/shared-paul-files/Webs/git-repos/ICTU---Digitale-Overheid-WP---rijksvideoplugin/mediaelement/' 
+rsync -r -a -v --delete '/Users/paul/shared-paul-files/Webs/temp/' '/Users/paul/shared-paul-files/Webs/git-repos/ICTU---Digitale-Overheid-WP---rijksvideoplugin/mediaelement/' 
 
 # remove temp dir
-rm -rfv '/shared-paul-files/Webs/temp/'
+rm -rfv '/Users/paul/shared-paul-files/Webs/temp/'
