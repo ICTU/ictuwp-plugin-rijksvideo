81f685a161430732366e39ce80b923a8075a59f4
Paul van Buuren <paul@wbvb.nl>
2026-04-29 12:04:00 +0200

    Renamed plugin according to conventions

--- ictuwp-plugin-rijksvideo.php
+++ ictuwp-plugin-rijksvideo.php
@@ -2,7 +2,7 @@
 /*
 // * Rijksvideo.
 // *
-// * Plugin Name:         ICTU / Rijksvideo digitaleoverheid.nl
+// * Plugin Name:         ICTU / WP / DO - Rijksvideo
 // * Plugin URI:          https://github.com/ICTU/digitale-overheid-wordpress-plugin-rijksvideoplugin/
 // * Description:         De mogelijkheid om video's in te voegen met diverse media-formats en ondertitels
 // * Version:             1.1.3
